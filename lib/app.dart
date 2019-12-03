848ba5a6dc2c97b193c5ddd896ffc08e330f0667
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-12-02 20:28:10 -0500

    added cupertino main function

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'styles.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,13 @@
-import 'dart:ffi';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
-import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-// import 'package:flutter/rendering.dart';
+import 'app.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // This app is designed only to work vertically, so we limit
+  // orientations to portrait up and down.
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      theme: ThemeData(primaryColor: Colors.white,),
-      home: RandomWords());
-  }
-}
-
-class RandomWordState extends State<RandomWords> {
-  final _suggestions = <WordPair>[]; //wordpair is imported at the top
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = Set<WordPair>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Random words'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            // Add 6 lines from here...
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          ); // ... to here.
-        },
-      ),
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*first call*/ (context, i) {
-        if (i.isOdd) return Divider(); /*second call*/
-
-        final index = i ~/ 2; /*third call*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*forth call*/
-        }
-
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordState createState() => RandomWordState();
+  return runApp(CupertinoStoreApp());
 }
